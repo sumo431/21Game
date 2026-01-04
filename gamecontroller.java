f4114d1ebcf0398562998d65e2cdb06695432ab9
sumo431 <sokada1@csuchico.edu>
2026-01-03 21:23:49 -0800

    compulete(maybe) game controller(background)

--- gamecontroller.java
+++ gamecontroller.java
@@ -8,5 +8,24 @@ public class GameController{
     public void start(@RequestParam int p){
         players = p;
         currentNumber = 0;
-    } 
+    }
+    @GetMapping("/turn")
+    public Map<String, Object> turn(){
+        int player = rand.nextInt(players);
+        int step = rand.nextInt(3) + 1;
+        boolen gameOver = false;
+        for(int i = 0; i < step; i++){
+            currentNumber++;
+            if(currentNumber == 21){
+                gameOver = true;
+                break;
+            }
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("player", player + 1);
+        response.put("step", step);
+        response.put("currentNumber", currentNumber);
+        response.put("gameOver", gameOver);
+        return response;
+    }
 }
